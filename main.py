3266c29bc2b9523c8828d4d2cfa09a36e59502f0
mick <anon@anon.com>
2023-04-09 18:46:47 -0400

    fixed error for explanation text

--- main.py
+++ main.py
@@ -169,22 +169,27 @@ for enum_1, data_file in enumerate(answer_files):
             q_num = int(match.group()[:len(match.group())-2])
             if ct == q_num:
                 answer = a_data[match.end():match.end()+1]
-                explanation = a_data[match.end()+3:]
+                next_q_pattern = re.compile(rf'{q_num+1}\. ')
+                next_q_match = next_q_pattern.search(a_data)
+                if next_q_match:
+                    explanation = a_data[match.end()+3:next_q_match.start()]
+                else:
+                    explanation = a_data[match.end()+3:]
                 q_dict = set_correct_answer(q_dict, q_num, str(enum_1+1), answer, explanation)
                 ct += 1
 
 # print(f'There are {chapter_ct} chapters in list')
 ct = 0
 while ct < chapter_ct:
-    # print(f'Chapter {ct+1}')
+    print(f'Chapter {ct+1}')
     q_lst = q_dict[str(ct+1)]
     for q in q_lst:
-        # print(q['q_num'], end=' ')
+        print(q['q_num'], end=' ')
         choice_list = q['choices']
         for c in choice_list:
             if c['is_correct']:
-                # print(c['opt'])
-                # print(c['explanation'])
+                print(c['opt'])
+                print(c['explanation'])
                 pass
     ct += 1
 
