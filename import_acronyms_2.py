4580eb66be0aa6a3fa10b7b6837a8115d49e22b9
mick <alfred.scott@protonmail.com>
2023-05-04 21:12:14 -0400

    update build_quiz()

--- import_acronyms_2.py
+++ import_acronyms_2.py
@@ -6,26 +6,14 @@ with open('acronyms_flash_data_2') as a:
 with open('acronym_flash_data_definitions_2') as d:
     definition_list = d.readlines()
 
-
-# dictionary layout
-# my_acronym_dict_list = [
-#     {
-#         'acronym': '',
-#         'definition': '',
-#         # will be set to 0, 1, or 2 by user with 0 being easiest, 2 being hardest
-#         'difficulty': '',
-#     }
-# ]
 acronym_dict = {}
 acronym_dict_list = []
 
 for i, acronym in enumerate(acronym_list):
     acronym_dict['acronym'] = acronym[0:len(acronym)-1]
     acronym_dict['definition'] = definition_list[i][0:len(definition_list[i])-1]
-    acronym_dict['difficulty'] = 2
+    acronym_dict['difficulty'] = 0
     acronym_dict['attempts'] = 0
-    # print(acronym, end=': ')
-    # print(definition_list[i])
     acronym_dict_list.append(acronym_dict.copy())
 
 for a_dict in acronym_dict_list:
--- quiz_acronyms.py
+++ quiz_acronyms.py
@@ -2,7 +2,7 @@ import json
 import random
 
 
-def make_banner(message):
+def banner(message):
     banner_len = 78
     mess_len = len(message)
     banner_diff = banner_len - mess_len
@@ -18,40 +18,62 @@ def make_banner(message):
         return message
 
 
-def build_quiz(complete_list, e, m, h, q_len):
-    random.shuffle(complete_list)
+def build_quiz(n_a, e, m, h, multiplier):
+    random.shuffle(n_a)
     random.shuffle(e)
     random.shuffle(m)
     random.shuffle(h)
 
-    q = []
+    num_n_a = 4 * multiplier
+    num_e = 1 * multiplier
+    num_m = 2 * multiplier
+    num_h = 3 * multiplier
 
-    if q_len == 10:
+    q = n_a[0:num_n_a]
+    print(f"len of q:        {len(q)}")
+    if len(e) < num_e:
+        print(f"length of e:     {len(e)}")
+        print(f"num needed:      {num_e}")
+        if len(e) != 0:
+            q.append(e)
+        num_to_pull = num_e - len(e)
+        print(f"needed from n_a: {num_to_pull}")
+        tmp_lst = n_a[len(q):len(q)+num_to_pull]
+        for a in tmp_lst:
+            q.append(a)
+    else:
+        tmp_list = e[0:num_e]
+        for a in tmp_list:
+            q.append(a)
+    if len(m) < num_m:
         pass
-    elif q_len == 20:
-        pass
-    elif q_len == 30:
-        pass
-    elif q_len == 40:
+    if len(h) < num_h:
         pass
 
+    for a in q:
+        print(a['acronym'])
+    print(f"len of q:        {len(q)}")
     return q
 
 
 with open('json/acronyms.json', 'r') as f:
     acronym_list = json.load(f)
 
+
 easy_list = []
 medium_list = []
 hard_list = []
+no_attempts_list =[]
 
 for acr_dict in acronym_list:
-    if acr_dict['difficulty'] == 0:
+    if acr_dict['difficulty'] == 1:
         easy_list.append(acr_dict.copy())
-    elif acr_dict['difficulty'] == 1:
+    elif acr_dict['difficulty'] == 2:
         medium_list.append((acr_dict.copy()))
-    else:
+    elif acr_dict['difficulty'] == 3:
         hard_list.append((acr_dict.copy()))
+    else:
+        no_attempts_list.append(acr_dict.copy())
 
 prompt_0_str = 'How many acronyms would you like to study?\n' \
                '\t1.)  10\n' \
@@ -60,25 +82,26 @@ prompt_0_str = 'How many acronyms would you like to study?\n' \
                '\t4.)  40\n\n' \
                '--> '
 
-print(make_banner('Acronyms Quiz'), end='\n\n')
+print(banner('Acronyms Quiz'), end='\n\n')
 
 prompt_0 = input(prompt_0_str)
-quiz_len = None
+# quiz_len = None
 
-while quiz_len is None:
-    if prompt_0 == '1':
-        quiz_len = 10
-        quiz = build_quiz(acronym_list, easy_list, medium_list, hard_list, 10)
-    elif prompt_0 == '2':
-        quiz_len = 20
-        quiz = build_quiz(acronym_list, easy_list, medium_list, hard_list, 20)
-    elif prompt_0 == '3':
-        quiz_len = 30
-        quiz = build_quiz(acronym_list, easy_list, medium_list, hard_list, 30)
-    elif prompt_0 == '4':
-        quiz_len = 40
-        quiz = build_quiz(acronym_list, easy_list, medium_list, hard_list, 40)
+while True:
+    if prompt_0 == '1' or '2' or '3' or '4':
+        # quiz_len = 10
+        quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, int(prompt_0))
+        break
+    # elif prompt_0 == '2':
+        # quiz_len = 20
+        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 20)
+    # elif prompt_0 == '3':
+        # quiz_len = 30
+        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 30)
+    # elif prompt_0 == '4':
+        # quiz_len = 40
+        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 40)
     else:
-        print(make_banner('Acronyms Quiz'), end='\n')
+        print(banner('Acronyms Quiz'), end='\n')
         print('\nInvalid Input - Please try again\n')
         prompt_0 = input(prompt_0_str)
