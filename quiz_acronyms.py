121233a986753a75b510735c938f69e8b4c1d801
mick <anon@anon.com>
2023-05-04 21:47:41 -0400

    build quiz functioning

--- quiz_acronyms.py
+++ quiz_acronyms.py
@@ -30,14 +30,15 @@ def build_quiz(n_a, e, m, h, multiplier):
     num_h = 3 * multiplier
 
     q = n_a[0:num_n_a]
+
     print(f"len of q:        {len(q)}")
+
     if len(e) < num_e:
-        print(f"length of e:     {len(e)}")
-        print(f"num needed:      {num_e}")
         if len(e) != 0:
-            q.append(e)
+            tmp_list = e[:len(e)]
+            for a in tmp_list:
+                q.append(a)
         num_to_pull = num_e - len(e)
-        print(f"needed from n_a: {num_to_pull}")
         tmp_lst = n_a[len(q):len(q)+num_to_pull]
         for a in tmp_lst:
             q.append(a)
@@ -45,14 +46,39 @@ def build_quiz(n_a, e, m, h, multiplier):
         tmp_list = e[0:num_e]
         for a in tmp_list:
             q.append(a)
+
     if len(m) < num_m:
-        pass
+        if len(m) != 0:
+            tmp_list = m[:len(m)]
+            for a in tmp_list:
+                q.append(a)
+        num_to_pull = num_m - len(m)
+        tmp_lst = n_a[len(q):len(q) + num_to_pull]
+        for a in tmp_lst:
+            q.append(a)
+    else:
+        tmp_list = m[0:num_m]
+        for a in tmp_list:
+            q.append(a)
+
     if len(h) < num_h:
-        pass
+        if len(h) != 0:
+            tmp_list = h[:len(h)]
+            for a in tmp_list:
+                q.append(a)
+        num_to_pull = num_h - len(h)
+        tmp_lst = n_a[len(q):len(q) + num_to_pull]
+        for a in tmp_lst:
+            q.append(a)
+    else:
+        tmp_list = h[0:num_h]
+        for a in tmp_list:
+            q.append(a)
 
     for a in q:
         print(a['acronym'])
-    print(f"len of q:        {len(q)}")
+
+    print('\n'+f"len of q:        {len(q)}")
     return q
 
 
@@ -63,7 +89,7 @@ with open('json/acronyms.json', 'r') as f:
 easy_list = []
 medium_list = []
 hard_list = []
-no_attempts_list =[]
+no_attempts_list = []
 
 for acr_dict in acronym_list:
     if acr_dict['difficulty'] == 1:
@@ -85,22 +111,11 @@ prompt_0_str = 'How many acronyms would you like to study?\n' \
 print(banner('Acronyms Quiz'), end='\n\n')
 
 prompt_0 = input(prompt_0_str)
-# quiz_len = None
 
 while True:
     if prompt_0 == '1' or '2' or '3' or '4':
-        # quiz_len = 10
         quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, int(prompt_0))
         break
-    # elif prompt_0 == '2':
-        # quiz_len = 20
-        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 20)
-    # elif prompt_0 == '3':
-        # quiz_len = 30
-        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 30)
-    # elif prompt_0 == '4':
-        # quiz_len = 40
-        # quiz = build_quiz(no_attempts_list, easy_list, medium_list, hard_list, 40)
     else:
         print(banner('Acronyms Quiz'), end='\n')
         print('\nInvalid Input - Please try again\n')
